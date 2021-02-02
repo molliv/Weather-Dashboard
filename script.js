f0620f2fbfe92fde9da456d9279d3e56cac025bc
Molli V <mollitalkin@hotmail.com>
2021-02-01 21:04:56 -0800

    updated html

--- script.js
+++ script.js
@@ -1,40 +1,55 @@
 $(document).ready(function() {
+
+  var APIKEY = "b44228de0fff9bb296f65506f3071a2f";
+
   $("#search-button").on("click", function() {
-    var searchValue = $("#search-value").val();
+    var cityID = $("#search-input").val();
 
     // clear input box
-    $("#search-value").val("");
+    $("#search-input").val("");
 
-    searchWeather(searchValue);
+    searchWeather(cityID);
   });
 
   $(".history").on("click", "li", function() {
     searchWeather($(this).text());
   });
 
-  var APIKEY = "b44228de0fff9bb296f65506f3071a2f";
-
   function makeRow(text) {
     var li = $("<li>").addClass("list-group-item list-group-item-action").text(text);
     $(".history").append(li);
   }
 
-  function searchWeather(searchValue) {
+  /*
+  function searchWeather(cityID) {
     $.ajax({
       type: "GET",
-      url: "http://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&appid=" + APIKEY + "&units=imperial",
+      url: "https://api.openweathermap.org/data/2.5/weather?q=" + cityID + "&appid=" + APIKEY, 
+      //+ "&units=imperial",
       dataType: "json",
       success: function(data) {
+  */
+  
+  function searchWeather(cityID) {
+    var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityID + "&appid=" + APIKEY;
+
+    fetch(requestUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+
         // create history link for this search
-        if (history.indexOf(searchValue) === -1) {
-          history.push(searchValue);
+        if (history.indexOf(cityID) === -1) {
+          history.push(cityID);
           window.localStorage.setItem("history", JSON.stringify(history));
     
-          makeRow(searchValue);
+          makeRow(cityID);
         }
         
         // clear any old content
-        $("#today").empty();
+        $("#current").empty();
 
         // create html content for current weather
         var title = $("<h3>").addClass("card-title").text(data.name + " (" + new Date().toLocaleDateString() + ")");
@@ -43,27 +58,38 @@ $(document).ready(function() {
         var humid = $("<p>").addClass("card-text").text("Humidity: " + data.main.humidity + "%");
         var temp = $("<p>").addClass("card-text").text("Temperature: " + data.main.temp + " °F");
         var cardBody = $("<div>").addClass("card-body");
-        var img = $("<img>").attr("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+        var img = $("<img>").attr("src", "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
 
         // merge and add to page
         title.append(img);
         cardBody.append(title, temp, humid, wind);
         card.append(cardBody);
-        $("#today").append(card);
+        $("#current").append(card);
 
         // call follow-up api endpoints
-        getForecast(searchValue);
+        getForecast(cityID);
         getUVIndex(data.coord.lat, data.coord.lon);
-      }
-    });
-  }
+      })
+    //});
+  };
   
-  function getForecast(searchValue) {
+  function getForecast(cityID) {
+    /*
     $.ajax({
       type: "GET",
-      url: "http://api.openweathermap.org/data/2.5/forecast?q=" + searchValue + "&appid=" + APIKEY + "&units=imperial",
+      url: "https://api.openweathermap.org/data/2.5/forecast?q=" + cityID + "&appid=" + APIKEY + "&units=imperial",
       dataType: "json",
       success: function(data) {
+        */
+    var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityID + "&appid=" + APIKEY;
+
+    fetch(requestUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+        
         // overwrite any existing content with title and empty row
         $("#forecast").html("<h4 class=\"mt-3\">5-Day Forecast:</h4>").append("<div class=\"row\">");
 
@@ -78,7 +104,7 @@ $(document).ready(function() {
 
             var title = $("<h5>").addClass("card-title").text(new Date(data.list[i].dt_txt).toLocaleDateString());
 
-            var img = $("<img>").attr("src", "http://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png");
+            var img = $("<img>").attr("src", "https://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png");
 
             var p1 = $("<p>").addClass("card-text").text("Temp: " + data.list[i].main.temp_max + " °F");
             var p2 = $("<p>").addClass("card-text").text("Humidity: " + data.list[i].main.humidity + "%");
@@ -88,16 +114,28 @@ $(document).ready(function() {
             $("#forecast .row").append(col);
           }
         }
-      }
-    });
-  }
+      })
+
+  };
 
   function getUVIndex(lat, lon) {
+    /*
     $.ajax({
       type: "GET",
-      url: "http://api.openweathermap.org/data/2.5/uvi?appid=" + APIKEY + "&lat=" + lat + "&lon=" + lon,
+      url: "https://api.openweathermap.org/data/2.5/uvi?appid=" + APIKEY + "&lat=" + lat + "&lon=" + lon,
       dataType: "json",
       success: function(data) {
+        */
+       
+      var requestUrl = "https://api.openweathermap.org/data/2.5/uvi?appid=" + APIKEY + "&lat=" + lat + "&lon=" + lon;
+   
+    fetch(requestUrl)
+      .then(function (response) {
+        return response.json();
+        })
+      .then(function (data) {
+        console.log(data);
+
         var uv = $("<p>").text("UV Index: ");
         var btn = $("<span>").addClass("btn btn-sm").text(data.value);
         
@@ -112,15 +150,15 @@ $(document).ready(function() {
           btn.addClass("btn-danger");
         }
         
-        $("#today .card-body").append(uv.append(btn));
-      }
+        $("#current .card-body").append(uv.append(btn));
+      })
 
       //bodyEl,appendChild(uvEl),
       //uvEl,appendChild(buttonEl),
-    });
-  }
+    }
+  })
 
-});
+//});
 
-document.querySelector("#search-button").addEventListener//("click", getSearchVal);
-("click", searchValue);
+document.querySelector("#search-button").addEventListener//("click", getcityID);
+//("click", cityID);
